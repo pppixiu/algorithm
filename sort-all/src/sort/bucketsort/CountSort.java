35728850b52a26a3672159785bbd9b32bb65ba81
apple <jiashuai.wang@ly.com>
2021-06-14 14:58:12 +0800

    RadixSort

--- sort-all/src/sort/bucketsort/CountSort.java
+++ sort-all/src/sort/bucketsort/CountSort.java
@@ -3,19 +3,19 @@ package sort.bucketsort;
 import sort.SortHelper;
 
 /**
- * stability
+ * stability with sort2
  *
  * @Classname BucketSort
  * @Description non
  * @Date 2021/6/13 5:26 下午
  * @Created by apple
  */
-public class BucketSort {
+public class CountSort {
 
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
             int[] arr = SortHelper.generatePositiveArr(100, 100);
-            sort(arr);
+            sort2(arr);
             SortHelper.judge(arr);
         }
     }
@@ -38,6 +38,38 @@ public class BucketSort {
             while (bucket[j]-- > 0) {
                 arr[i++] = j;
             }
+        }
+    }
+
+    /**
+     * stability
+     * @param arr
+     */
+    public static void sort2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        int[] bucket = new int[max + 1];
+        for (int i = 0; i < arr.length; i++) {
+            bucket[arr[i]]++;
+        }
+
+
+        for (int i = 0; i < bucket.length - 1; i++) {
+            bucket[i + 1] += bucket[i];
+        }
+
+        int[] help = new int[arr.length];
+        for (int j = arr.length - 1; j >= 0; j--) {
+            help[--bucket[arr[j]]] = arr[j];
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = help[i];
         }
     }
 }
--- sort-all/src/sort/bucketsort/RadixSort.java
+++ sort-all/src/sort/bucketsort/RadixSort.java
@@ -1,10 +1,76 @@
 package sort.bucketsort;
 
+import sort.SortHelper;
+
 /**
+ * stability
+ *
  * @Classname RaeixSort
  * @Description non
  * @Date 2021/6/14 1:58 下午
  * @Created by apple
  */
 public class RadixSort {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            int[] arr = SortHelper.generatePositiveArr(100, 100);
+            sort(arr);
+            SortHelper.judge(arr);
+        }
+    }
+
+    public static void sort(int arr[]) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        radixSort(arr, 0, arr.length - 1, maxbits(arr));
+    }
+
+    public static void radixSort(int[] arr, int l, int r, int digit) {
+        int radix = 10;
+        int i = 0;
+        int j = 0;
+        int[] help = new int[r - l + 1];
+
+        for (int d = 1; d <= digit; d++) {
+            int[] count = new int[radix];
+            for (int c = 0; c < arr.length; c++) {
+                count[getDigit(arr[c], d)]++;
+            }
+
+            for (int c = 1; c < count.length; c++) {
+                count[c] += count[c - 1];
+            }
+
+            for (int c = r; c >= l; c--) {
+                int nowDigit = getDigit(arr[c], d);
+                int position = --count[nowDigit];
+                help[position] = arr[c];
+            }
+
+            for (i = l, j = 0; i <= r; i++, j++) {
+                arr[i] = help[j];
+            }
+        }
+    }
+
+    public static int maxbits(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        int res = 0;
+        while (max != 0) {
+            res++;
+            max /= 10;
+        }
+        return res;
+    }
+
+    public static int getDigit(int x, int d) {
+        return ((x / ((int) Math.pow(10, d - 1))) % 10);
+    }
+
 }
