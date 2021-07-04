eabf10f3080045c421c758d9f75cdea24de24210
apple <jiashuai.wang@ly.com>
2021-07-04 16:05:33 +0800

    about linkedlist

---
+++ about-linked-list/src/linkedlist/LInkedListPartition.java
@@ -0,0 +1,105 @@
+package linkedlist;
+
+/**
+ * Given the head of a linked list and a value x,
+ * partition it such that all nodes less than x come before nodes greater than or equal to x.
+ * <p>
+ * You should preserve the original relative order of the nodes in each of the two partitions.
+ *
+ * @Classname LInkedListPartition
+ * @Description non
+ * @Date 2021/7/4 3:09 下午
+ * @Created by apple
+ */
+public class LInkedListPartition {
+
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        Node head1 = new Node(7);
+        head1.next = new Node(9);
+        head1.next.next = new Node(1);
+        head1.next.next.next = new Node(8);
+        head1.next.next.next.next = new Node(5);
+        head1.next.next.next.next.next = new Node(2);
+        head1.next.next.next.next.next.next = new Node(5);
+        printLinkedList(head1);
+        // head1 = listPartition1(head1, 4);
+        head1 = partition(head1, 5);
+        printLinkedList(head1);
+
+    }
+
+    public static void printLinkedList(Node node) {
+        System.out.print("Linked List: ");
+        while (node != null) {
+            System.out.print(node.value + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+
+    public static Node partition(Node head, int pivot) {
+        Node smallHead = null;
+        Node smallTail = null;
+        Node equalHead = null;
+        Node equalTail = null;
+        Node bigHead = null;
+        Node bigTail = null;
+
+        Node next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = null;
+            if (head.value < pivot) {
+                if (smallHead == null) {
+                    smallHead = head;
+                    smallTail = head;
+                } else {
+                    smallTail.next = head;
+                    smallTail = head;
+                }
+            } else if (head.value == pivot) {
+                if (equalHead == null) {
+                    equalHead = head;
+                    equalTail = head;
+                } else {
+                    equalTail.next = head;
+                    equalTail = head;
+                }
+            } else {
+                if (bigHead == null) {
+                    bigHead = head;
+                    bigTail = head;
+                } else {
+                    bigTail.next = head;
+                    bigTail = head;
+                }
+            }
+            head = next;
+        }
+
+        if (smallTail != null) {
+            smallTail.next = equalHead;
+            equalTail = equalTail == null ? smallTail : equalTail;
+        }
+
+        if (equalTail != null) {
+            equalTail.next = bigHead;
+        }
+
+        return smallHead != null ? smallHead : equalHead != null ? equalHead : bigHead;
+    }
+
+
+}
